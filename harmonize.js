5ca339e057032d88254ab9e3e228ba9d8d211b62
Daiderd Jordan <daiderd@gmail.com>
2015-03-13 17:37:29 +0100

    fix for node v0.12.0

--- harmonize.js
+++ harmonize.js
@@ -31,11 +31,11 @@ module.exports = function() {
         }
 
         // harmony flag is unnecessary in io and beginning with node v0.12
-        if(isIojs || (!isIojs && v[0] == 0 && v[1] >= 12)) {
+        if(isIojs || (!isIojs && v[0] == 0 && v[1] > 12)) {
             return;
         }
 
-        var node = child_process.spawn(process.argv[0], ['--harmony'].concat(process.argv.slice(1)), {});
+        var node = child_process.spawn(process.argv[0], ['--harmony', '--harmony-proxies'].concat(process.argv.slice(1)), {});
         node.stdout.pipe(process.stdout);
         node.stderr.pipe(process.stderr);
         node.on("close", function(code) {
